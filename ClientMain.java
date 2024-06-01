c83ea34f40eb15b8ace305a9c58633412dda18cf
kji7666 <165553440+kji7666@users.noreply.github.com>
2024-06-01 15:00:51 +0800

    clean

--- ClientMain.java
+++
@@ -1,8 +0,0 @@
-import bcProject.FTP.FTPClient;
-
-public class ClientMain {
-    public static void main(String[] args){
-        FTPClient client = new FTPClient();
-        client.startConnecting();
-    }
-}
